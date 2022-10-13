62f2c0ee0e88335bdfac405f410e4241f48d2c8c
Rodion Sviridov <sviridov.rod@yandex.ru>
2022-10-13 20:45:04 +0300

    Practice two moved to _old directory. Practice 3 done-ish

--- db/init.sql
+++ db/init.sql
@@ -1,42 +1,39 @@
-CREATE DATABASE IF NOT EXISTS appDB;
+CREATE DATABASE IF NOT EXISTS appDb;
 CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
-GRANT SELECT,UPDATE,INSERT ON appDB.* TO 'user'@'%';
+GRANT ALL PRIVILEGES ON appDb.* TO 'user'@'%';
 FLUSH PRIVILEGES;
+USE appDb;
 
-USE appDB;
 CREATE TABLE IF NOT EXISTS users (
-  ID INT(11) NOT NULL AUTO_INCREMENT,
-  name VARCHAR(20) NOT NULL,
-  surname VARCHAR(40) NOT NULL,
-  PRIMARY KEY (ID)
-);
+    ID INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) CHARACTER SET ascii NOT NULL,
+    password VARCHAR(45) CHARACTER SET ascii NOT NULL,
+    PRIMARY KEY (ID)
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Alex', 'Rover') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Alex' AND surname = 'Rover'
-) LIMIT 1;
+CREATE TABLE IF NOT EXISTS valuables (
+    ID INT(10) NOT NULL AUTO_INCREMENT,
+    title VARCHAR(32) NOT NULL,
+    description VARCHAR(256) NOT NULL,
+    cost INT(6) NOT NULL,
+    PRIMARY KEY (ID)
+    );
 
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Bob', 'Marley') AS tmp
+-- htpasswd -bns admin admin
+INSERT INTO users (name, password)
+SELECT * FROM (SELECT 'admin', '{SHA}0DPiKuNIrrVmD8IUCuw1hQxNqZc=') AS temp
 WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Bob' AND surname = 'Marley'
-) LIMIT 1;
+        SELECT name FROM users WHERE name = 'admin' AND password = '{SHA}0DPiKuNIrrVmD8IUCuw1hQxNqZc='
+    ) LIMIT 1;
 
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Alex', 'Rover') AS tmp
+INSERT INTO valuables (title, description, cost)
+SELECT * FROM (SELECT 'Vase', 'Antique chinese flower vase from XIX century', 1000) AS temp
 WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Alex' AND surname = 'Rover'
-) LIMIT 1;
+        SELECT title FROM valuables WHERE title = 'Vase' AND description = 'Antique chinese flower vase from XIX century' AND cost = 1000
+    ) LIMIT 1;
 
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Kate', 'Yandson') AS tmp
+INSERT INTO valuables (title, description, cost)
+SELECT * FROM (SELECT 'Chair', 'Cheap wooden chair', 50) AS temp
 WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Kate' AND surname = 'Yandson'
-) LIMIT 1;
-
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Lilo', 'Black') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Lilo' AND surname = 'Black'
-) LIMIT 1;
+        SELECT title FROM valuables WHERE title = 'Chair' AND description = 'Cheap wooden chair' AND cost = 50
+    ) LIMIT 1;
