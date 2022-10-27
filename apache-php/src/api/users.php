d01b3b3c3fe4bcab23112deac52c86de438d5ca0
Rodion Sviridov <sviridov.rod@yandex.ru>
2022-10-27 15:08:41 +0300

    No password user search

--- apache-php/src/api/users.php
+++ apache-php/src/api/users.php
@@ -4,8 +4,8 @@ require_once '../utils.php';
 
 switch ($_SERVER['REQUEST_METHOD']) {
 	case 'GET':
-		$params = array_filter($_GET, 'filter_user_get_patch', ARRAY_FILTER_USE_KEY);
-		if (count($params) > 3) {
+		$params = array_filter($_GET, 'filter_user_get', ARRAY_FILTER_USE_KEY);
+		if (count($params) > 2) {
 			http_response_code(400);
 			echo "Bad request: invalid request params";
 			return;
@@ -25,7 +25,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
 		break;
 	case 'PATCH':
 		$body = json_decode(file_get_contents('php://input'), true);
-		$params = array_filter($body, 'filter_user_get_patch', ARRAY_FILTER_USE_KEY);
+		$params = array_filter($body, 'filter_user_patch', ARRAY_FILTER_USE_KEY);
 		if (!array_key_exists('id', $params) || count($params) == 0 || count($params) > 3) {
 			http_response_code(400);
 			echo "Bad request: invalid request params";
@@ -46,8 +46,10 @@ switch ($_SERVER['REQUEST_METHOD']) {
 		}
 		break;
 }
-
-function filter_user_get_patch($key): bool {
+function filter_user_get($key): bool {
+	return $key == 'id' || $key == 'name';
+}
+function filter_user_patch($key): bool {
 	return $key == 'id' || $key == 'name' || $key == 'password';
 }
 function filter_user_post($key): bool {
