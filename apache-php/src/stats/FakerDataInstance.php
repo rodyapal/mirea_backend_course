e47d723c79d37a29b58f64891d5c92ceda1975a5
Rodion Sviridov <sviridov.rod@yandex.ru>
2022-12-07 11:13:06 +0300

    Practice 6 done

---
+++ apache-php/src/stats/FakerDataInstance.php
@@ -0,0 +1,25 @@
+<?php
+
+class FakerDataInstance
+{
+    public string $name;
+    public string $full_name;
+    public string $email;
+    public string $gender;
+    public string $bloodType;
+
+    public function __construct(
+        $name,
+        $full_name,
+        $email,
+        $gender,
+        $bloodType
+    )
+    {
+        $this->name = $name;
+        $this->full_name = $full_name;
+        $this->email = $email;
+        $this->gender = $gender;
+        $this->bloodType = $bloodType;
+    }
+}
